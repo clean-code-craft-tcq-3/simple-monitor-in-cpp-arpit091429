806b0a8bb0ddf443fc7df40b36d2c84617cde456
arpit091429 <arpitkhandelwal24@gmail.com>
2022-05-06 18:39:03 +0530

    Create NewBatteryState.h

---
+++ NewBatteryState.h
@@ -0,0 +1,39 @@
+#include "Battery.h"
+class NewBatteryState : public Battery
+{
+  public:
+    bool checkTempRange(float temperature)
+    {
+        if (temperature < 0 || temperature > 45)
+        {
+            cout << "Temperature out of range!\n";
+            return false;
+        }
+        else
+            return true;
+    }
+    bool checkSocRange(float soc)
+    {
+        if (soc < 20 || soc > 80)
+        {
+            cout << "State of Charge out of range!\n";
+            return false;
+        }
+        else
+            return true;
+    }
+    bool checkChargeRateRange(float chargeRate)
+    {
+        if (chargeRate > 0.8)
+        {
+            cout << "Charge Rate out of range!\n";
+            return false;
+        }
+        else
+            return true;
+    }
+    bool checkBatteryIsOk(float t, float s, float c)
+    {
+        return (checkTempRange(t) && checkSocRange(s) && checkChargeRateRange(c));
+    }
+};
