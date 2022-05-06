fb1810629ea07d95219c7f96c7e452f2442d640d
arpit091429 <arpitkhandelwal24@gmail.com>
2022-05-06 18:38:26 +0530

    Create TestBattery.h

---
+++ TestBattery.h
@@ -0,0 +1,25 @@
+#include "NewBatteryState.h"
+#include <assert.h>
+
+void testBattery()
+{
+    NewBatteryState n;
+    // temperature test
+    assert(n.checkBatteryIsOk(0, 30, 0.5) == true);
+    assert(n.checkBatteryIsOk(-1, 30, 0.5) == false);
+    assert(n.checkBatteryIsOk(45, 30, 0.5) == true);
+    assert(n.checkBatteryIsOk(46, 30, 0.5) == false);
+    assert(n.checkBatteryIsOk(30, 30, 0.5) == true);
+
+    // SOC test
+    assert(n.checkBatteryIsOk(45, 19, 0.5) == false);
+    assert(n.checkBatteryIsOk(45, 20, 0.5) == true);
+    assert(n.checkBatteryIsOk(45, 80, 0.5) == true);
+    assert(n.checkBatteryIsOk(45, 81, 0.5) == false);
+    assert(n.checkBatteryIsOk(45, 50, 0.5) == true);
+
+    // SOC test
+    assert(n.checkBatteryIsOk(45, 50, 0.8) == false);
+    assert(n.checkBatteryIsOk(45, 50, 0.9) == false);
+    assert(n.checkBatteryIsOk(45, 50, 0.7) == true);
+}
